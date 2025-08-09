c8620148c61481d2c8d69f1b76910ef16763ac02
Siran <149071374+siran-P@users.noreply.github.com>
2025-08-09 09:43:03 +0530

    My practice programs

---
+++ predefinedMacros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Date:%s",__DATE__);
+    printf("\nTime:%s",__TIME__);
+    printf("\nFile:%s",__FILE__);
+    printf("\nLine:%d",__LINE__);
+    printf("\nANSI:%d",__STDC__);
+    return 0;
+}
